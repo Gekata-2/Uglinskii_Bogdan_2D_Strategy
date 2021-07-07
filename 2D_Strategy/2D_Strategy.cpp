0679d11a23010d538ea2c2f96e814542112b46ae
Gekata-2 <sosorii2002@gmail.com>
2021-07-07 20:18:11 +0300

    Removed some training stuff

--- 2D_Strategy/2D_Strategy.cpp
+++ 2D_Strategy/2D_Strategy.cpp
@@ -1,59 +1,12 @@
 #include <iostream>
 #include <vector>
 #include "TBattleground.h"
-using namespace std;
-class Parent
-{
-public:
-    virtual void foo()
-    {
-        cout << "parent is called";
-    }
-};
 
-class archer : public Parent
-{
-public:
-    virtual void foo()
-    {
-        cout << "archer is called";
-    }
-};
-class sword : public Parent
-{
-public:
-    int a = 11;
-    virtual void foo()
-    {
-        cout << "sword is called";
-    }
-    void set_a(int tmp)
-    {
-        a = tmp;
-    }
-};
 
-class map
-{
-    Parent* arr[10];
-   
-    
-public:
-    void asd(sword* sw)
-    {
-        sw->set_a(100);
-        std::cout << sw->a<<std::endl;
-    }
-    
-};
 
 int main()
 {
-    map mp;
-    sword s;
-    std::cout << s.a << std::endl;
-    mp.asd(&s);
-    std::cout << s.a << std::endl;
+   
 
     TBattleground bg;
     std::vector<TSwordsman> sw_vec;
